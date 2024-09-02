a25d3795395b1ac6539d2ecc328f90b4b0b0bd69
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-09-03 01:07:34 +0600

    string with play slice concat includes join  split slice

---
+++ String With play/Slice.js
@@ -0,0 +1,43 @@
+const onakBoro = "abdullahAlMahfuj";
+
+// console.log(onakBoro.slice(5, 8)) 
+
+
+
+
+
+const sentence = "I am a good programer and herd worker";
+
+
+// console.log(sentence.split("a"))
+
+const friends = "aslam,nazim,sani,rohol,tamim,shohag";
+
+// console.log(friends.split(","))
+
+
+
+
+
+const goodFriends = ["Sabbir", "Nazim", "Tamim", "Rohol", "Sani", "Aslam"];
+
+// console.log(goodFriends.join())
+// console.log(goodFriends.join(","))
+// console.log(goodFriends.join("|"))
+
+
+
+
+
+const firstName = "Sabbir";
+const lastName = "Hosen"
+
+// console.log(firstName + " " + lastName)
+// console.log(firstName.concat().concat(lastName))
+// console.log(firstName.concat(" ").concat(lastName))
+
+
+const random = "Hi this is sabbir";
+
+// console.log(random.includes("b"));
+// console.log(random.includes("x"));
