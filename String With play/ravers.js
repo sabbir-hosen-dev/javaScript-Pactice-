e55dab3068c576207a9f26ac1d88064770ef3ac5
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-09-03 01:37:49 +0600

    sentex revers for for of while ane sentex use

---
+++ String With play/ravers.js
@@ -0,0 +1,40 @@
+const sentence = "I am lerning programing";
+
+
+// for of loop 
+let revers = " ";
+for (const letter of sentence) {
+  revers = letter + revers;
+}
+console.log(revers);
+
+
+
+
+// for loop 
+let rev = "";
+
+for (let i = 0; i < sentence.length; i++) {
+  rev = sentence[i] + rev;
+}
+
+console.log(rev)
+
+
+
+// while loot 
+let i = 0;
+let rererssent = '';
+while( i < sentence.length) {
+  rererssent = sentence[i] + rererssent
+
+  i++
+}
+console.log(rererssent)
+
+
+
+// string 
+const reversSentex = sentence.split('').reverse().join("");
+
+console.log(reversSentex)
